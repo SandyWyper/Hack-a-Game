4ad4a3e7fc1c567a55b1f0a581aa0130d0dd84ae
SandyThatWorks <sandy@thatworks.agency>
2025-06-19 15:56:56 +0100

    Refactor

---
+++ assets/scripts/script.js
@@ -0,0 +1,38 @@
+import { doConfetti } from "./confetti.js";
+import { getImage } from "./getImage.js";
+import { cats, dogs } from "./catsndogs.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // console.log(cats, dogs);
+  const startLayout = document.getElementById("start-layout");
+  const quizLayout = document.getElementById("quiz-layout");
+  const endLayout = document.getElementById("end-layout");
+  const startBtn = document.getElementById("start-button");
+  const quizBtns = document.querySelectorAll(".answer-button");
+  const retryBtn = document.getElementById("retry-button");
+
+  function showLayout(layoutToShow) {
+    startLayout.style.display = "none";
+    quizLayout.style.display = "none";
+    endLayout.style.display = "none";
+    layoutToShow.style.display = "block";
+    // Footer is always visible, so no need to toggle it
+  }
+
+  // Show start-layout at the beginning
+  showLayout(startLayout);
+
+  startBtn.addEventListener("click", () => {
+    showLayout(quizLayout);
+  });
+
+  quizBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      showLayout(endLayout);
+    });
+  });
+
+  retryBtn.addEventListener("click", () => {
+    showLayout(quizLayout);
+  });
+});
