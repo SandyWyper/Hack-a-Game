45234accfbf171cd39b933637a7c5b0c3c2a7cb5
SandyThatWorks <sandy@thatworks.agency>
2025-06-21 08:03:00 +0100

    FEAT; Allow for cats or dogs when creating question

--- assets/scripts/generateQuestion.js
+++ assets/scripts/generateQuestion.js
@@ -1,27 +1,29 @@
-import { cats } from "./catsndogs.js";
+import { cats, dogs } from "./catsndogs.js";
 import { getImage } from "./getImage.js";
 
-export async function generateQuestion() {
-  
-  const correctIndex = Math.floor(Math.random() * cats.length);
-  const correctBreed = cats[correctIndex];
+export async function generateQuestion(animalType) {
+  const petSpecies = animalType === "dogs" ? dogs : cats;
+  const correctIndex = Math.floor(Math.random() * petSpecies.length);
+  const correctBreed = petSpecies[correctIndex];
 
-  
   let incorrectOptions = [];
   while (incorrectOptions.length < 2) {
-    const randomIndex = Math.floor(Math.random() * cats.length);
-    if (randomIndex !== correctIndex && !incorrectOptions.includes(cats[randomIndex])) {
-      incorrectOptions.push(cats[randomIndex]);
+    const randomIndex = Math.floor(Math.random() * petSpecies.length);
+    if (
+      randomIndex !== correctIndex &&
+      !incorrectOptions.includes(petSpecies[randomIndex])
+    ) {
+      incorrectOptions.push(petSpecies[randomIndex]);
     }
   }
 
   //Shuffle options
-  const options = [correctBreed, ...incorrectOptions].sort(() => Math.random() - 0.5);
+  const options = [correctBreed, ...incorrectOptions].sort(
+    () => Math.random() - 0.5
+  );
 
-  
-  const imageURL = await getImage(correctBreed, "cats");
+  const imageURL = await getImage(correctBreed, animalType);
 
-  
   return {
     imageURL,
     options,
